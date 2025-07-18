e20368f8a9bdf9790dac645c0ac78eeedb1194f8
2-Eung <eung4256@gmail.com>
2025-07-18 14:39:22 +0900

    start

---
+++ src/test/java/com/example/untitled34/EungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.untitled34;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
